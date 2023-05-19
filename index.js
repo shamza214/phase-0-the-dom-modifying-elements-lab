590944f582231a8c28d399368078bb9bda319ca7
Syed Hamza <syedr.hamza@gmail.com>
2023-05-19 02:09:36 -0400

    final

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ document.getElementById('main').remove();
 
 const newHeader = document.createElement('h1')
 newHeader.id = 'victory'
-newHeader.innerHTML = "YOUR-NAME is the champion"
+newHeader.innerHTML = "Syed is the champion"
 
 
 /*
 <h1></h1>
 <h1 id="victory"></h1>
-<h1 id="victory">YOUR-NAME is the champion</h1>
+<h1 id="victory">Syed is the champion</h1>
 
 
 */
